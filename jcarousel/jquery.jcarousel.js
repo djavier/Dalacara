fb9af89352a58b471d90fba24a7f06f11b8ab557
Dariel Javier <dariel.javier@theberrycompany.com>
2013-04-11 11:57:38 -0400

    commented $.browser deprecated function on JCarousel

--- jcarousel/jquery.jcarousel.js
+++ jcarousel/jquery.jcarousel.js
@@ -191,12 +191,13 @@
             this.options.initCallback(this, 'init');
         }
 
-        if (!windowLoaded && $.browser.safari) {
-            this.buttons(false, false);
-            $(window).bind('load.jcarousel', function() { self.setup(); });
-        } else {
+        //Code commented because .browser.safari is deprecated on 1.9+
+        //if (!windowLoaded && $.browser.safari) {
+        //    this.buttons(false, false);
+        //    $(window).bind('load.jcarousel', function() { self.setup(); });
+        //} else {
             this.setup();
-        }
+       // }
     };
 
     // Create shortcut for internal use
